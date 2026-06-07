e1674ca2666e8283050e7d74d0f4f98d72987b24
kalelle-mae <kalellemaevictorio@gmail.com>
2026-06-07 22:58:38 +0800

    feat: add registerCustomer method to CustomerService

--- src/main/java/com/pup/taguig/app/service/CustomerService.java
+++ src/main/java/com/pup/taguig/app/service/CustomerService.java
@@ -1,5 +1,9 @@
 package com.pup.taguig.app.service;
 
+import com.pup.taguig.app.dto.CustomerResponseDTO;
+
 public interface CustomerService {
 
+	public CustomerResponseDTO registerCustomer(Long id);
+	
 }
