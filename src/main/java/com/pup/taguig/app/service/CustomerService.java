311a1b9e5bc391d00e8a256820ae6a7e56f74e4f
kalelle-mae <kalellemaevictorio@gmail.com>
2026-06-07 23:02:35 +0800

    refactor: use CustomerRequestDTO in registerCustomer service method

--- src/main/java/com/pup/taguig/app/service/CustomerService.java
+++ src/main/java/com/pup/taguig/app/service/CustomerService.java
@@ -1,9 +1,9 @@
 package com.pup.taguig.app.service;
 
-import com.pup.taguig.app.dto.CustomerResponseDTO;
+import com.pup.taguig.app.dto.CustomerRequestDTO;
 
 public interface CustomerService {
 
-	public CustomerResponseDTO registerCustomer(Long id);
+	public Long registerCustomer(CustomerRequestDTO request);
 	
 }
