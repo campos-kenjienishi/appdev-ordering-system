4c422bc4b151df7f542e0b7e4b45d226d968e3d2
kalelle-mae <kalellemaevictorio@gmail.com>
2026-06-07 22:47:53 +0800

    feat: create Customer model with name, email, and phone fields

--- src/main/java/com/pup/taguig/app/model/Customer.java
+++ src/main/java/com/pup/taguig/app/model/Customer.java
@@ -1,5 +1,16 @@
 package com.pup.taguig.app.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer {
 
+	private String name;
+	private String email;
+	private String phone;
+	
 }
