a3e75952550f8c64b66da180956297601a2328d4
kalelle-mae <kalellemaevictorio@gmail.com>
2026-06-07 22:52:58 +0800

    feat: create CustomerRequestDTO with name, email, and phone fields

--- src/main/java/com/pup/taguig/app/dto/CustomerRequestDTO.java
+++ src/main/java/com/pup/taguig/app/dto/CustomerRequestDTO.java
@@ -1,5 +1,16 @@
 package com.pup.taguig.app.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CustomerRequestDTO {
 
+	private String name;
+	private String email;
+	private String phone;
+	
 }
