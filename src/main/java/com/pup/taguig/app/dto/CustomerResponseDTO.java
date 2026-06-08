9cc75cdf3184bfc13e38b2b2a2c77087ad2e8072
kalelle-mae <kalellemaevictorio@gmail.com>
2026-06-08 09:43:11 +0800

    feat: add @AllArgsConstructor to CustomerResponseDTO

--- src/main/java/com/pup/taguig/app/dto/CustomerResponseDTO.java
+++ src/main/java/com/pup/taguig/app/dto/CustomerResponseDTO.java
@@ -1,5 +1,6 @@
 package com.pup.taguig.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CustomerResponseDTO {
 
 	private Long id;
