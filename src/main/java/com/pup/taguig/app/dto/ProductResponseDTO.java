595ea7d86273f06160f1c2d965c979dcf83abe77
miguel-kianbenedict <11bell.miguelkian@gmail.com>
2026-06-11 16:18:44 +0800

    unfinised product

--- src/main/java/com/pup/taguig/app/dto/ProductResponseDTO.java
+++ src/main/java/com/pup/taguig/app/dto/ProductResponseDTO.java
@@ -1,5 +1,21 @@
 package com.pup.taguig.app.dto;
 
-public class ProductResponseDTO {
+import java.util.List;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class ProductResponseDTO {
+@Getter
+@Setter
+@NoArgsConstructor
+	
+	private String name;
+	private String description;
+	private float price;
+	private int stock;
+	
+	private List<ProductResponse> products;
+	
 }
--- src/main/java/com/pup/taguig/app/model/Product.java
+++ src/main/java/com/pup/taguig/app/model/Product.java
@@ -1,5 +1,21 @@
 package com.pup.taguig.app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
+	
+	private String name;
+	private String description;
+	private float price;
+	private int stock;
+
 
 }
